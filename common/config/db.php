afd45816b181879355e0da52b8c9155be0e9c27d
pdwjun <pdwjun@gmail.com>
2015-01-23 14:41:12 +0800

    init

---
+++ common/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2-blog',
+    'username' => 'jason',
+    'password' => 'lrc207107',
+    'charset' => 'utf8',
+    'tablePrefix' => 'yii2_start_'
+];
